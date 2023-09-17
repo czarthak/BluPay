c434ecd0e2c37692f164ead14ff2e0405d0e2188
anirudhchinta03 <anirudhchinta03@vt.edu>
2023-09-17 02:58:37 -0400

    login page integrated

--- app/src/main/java/com/example/blupay/MainActivity.kt
+++ app/src/main/java/com/example/blupay/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.blupay
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.blupay.databinding.ActivityMainBinding
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.example.blupay.R
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var signOutBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+        signOutBtn = findViewById(R.id.signOutBtn)
 
-
+        signOutBtn.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, PhoneActivity::class.java))
+        }
     }
 }
